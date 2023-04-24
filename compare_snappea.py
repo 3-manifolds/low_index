bb70bf0095979d9ab9e8dcb57ee66513d88a22a5
Nathan Dunfield <nathan@dunfield.info>
2023-04-23 20:29:40 -0500

    snappea comparison: manually set the number of short relators

--- compare_snappea.py
+++ compare_snappea.py
@@ -2,22 +2,23 @@ import time
 import snappy
 import low_index
 
-def covers_low_index(self, degree, num_threads=0):
+def covers_low_index(self, degree, num_short_relators=0, num_threads=0):
     """
     Compute all covers using low_index.  Copied from
     triangulations.pyx so we can set the number of threads.
     """
     G = self.fundamental_group()
 
-    if G.num_relators() > self.num_cusps():
-        num_long_relators = self.num_cusps()
-        num_short_relators = G.num_relators() - num_long_relators
-        relators = sorted(G.relators(as_int_list=True), key = len)
-        short_relators = relators[:num_short_relators]
-        long_relators = relators[num_short_relators:]
-    else:
-        short_relators = G.relators(as_int_list = True)
-        long_relators = []
+    relators = sorted(G.relators(as_int_list=True), key=len)
+
+    
+    #if G.num_relators() > self.num_cusps():
+    #    num_short_relators = G.num_relators() - self.num_cusps()
+    #else:
+    #    num_short_relators = G.num_relators()
+
+    short_relators = relators[:num_short_relators]
+    long_relators = relators[num_short_relators:]
 
     return [self.cover(H)
             for H in low_index.permutation_reps(
@@ -27,7 +28,7 @@ def covers_low_index(self, degree, num_threads=0):
             if len(H[0]) == degree]
 
 
-def compare_methods(manifold, num_threads=4):
+def compare_methods(manifold, num_short_relators=0, num_threads=4):
     """
     Compare running time of low_index versus the snappea kernel.  For
     any fixed manifold, low_index will always win in the end, so
@@ -42,7 +43,7 @@ def compare_methods(manifold, num_threads=4):
         snappea_time = time.time() - start
 
         start = time.time()
-        covers = covers_low_index(M, degree, num_threads)
+        covers = covers_low_index(M, degree, num_short_relators, num_threads)
         low_index_time = time.time() - start
 
         a = min(snappea_time, low_index_time)
