586794edfbc59d871a26bb33722a3eb45c5e8eb5
Nathan Dunfield <nathan@dunfield.info>
2023-04-25 11:01:39 -0500

    Linux CI: switch to Ubuntu 20.04

--- compare_snappea.py
+++ compare_snappea.py
@@ -2,7 +2,7 @@ import time
 import snappy
 import low_index
 
-def covers_low_index(self, degree, num_short_relators=0, num_threads=0):
+def covers_low_index(self, degree, num_threads=0):
     """
     Compute all covers using low_index.  Copied from
     triangulations.pyx so we can set the number of threads.
@@ -11,24 +11,28 @@ def covers_low_index(self, degree, num_short_relators=0, num_threads=0):
 
     relators = sorted(G.relators(as_int_list=True), key=len)
 
+    short_relators, long_relators = [], relators
+    strategy = ''
     
-    #if G.num_relators() > self.num_cusps():
-    #    num_short_relators = G.num_relators() - self.num_cusps()
-    #else:
-    #    num_short_relators = G.num_relators()
-
-    short_relators = relators[:num_short_relators]
-    long_relators = relators[num_short_relators:]
-
+    if relators:
+        if len(relators[0]) <= 2*degree:
+            short_relators = [rel for rel in relators if len(rel) <= 2*degree]
+            long_relators = relators[len(short_relators):]
+            strategy = 'spin_short'
+        elif len(relators[0]) <= 3*degree:
+            short_relators = [rel for rel in relators if len(rel) <= 3*degree]
+            long_relators = relators[len(short_relators):]
+            
+    print(len(short_relators), strategy)
     return [self.cover(H)
             for H in low_index.permutation_reps(
                     G.num_generators(),
                     short_relators, long_relators,
-                    degree, num_threads=num_threads)
+                    degree, strategy='', num_threads=num_threads)
             if len(H[0]) == degree]
 
 
-def compare_methods(manifold, num_short_relators=0, num_threads=4):
+def compare_methods(manifold, num_threads=4):
     """
     Compare running time of low_index versus the snappea kernel.  For
     any fixed manifold, low_index will always win in the end, so
@@ -37,15 +41,15 @@ def compare_methods(manifold, num_short_relators=0, num_threads=4):
     M = manifold
     G = M.fundamental_group()
     print(M)
-    for degree in range(2, 10):
+    for degree in range(2, 8):
         start = time.time()
         covers = M.covers(degree, method='snappea')
         snappea_time = time.time() - start
 
         start = time.time()
-        covers = covers_low_index(M, degree, num_short_relators, num_threads)
+        covers = covers_low_index(M, degree, num_threads)
         low_index_time = time.time() - start
-
+        
         a = min(snappea_time, low_index_time)
         b = max(snappea_time, low_index_time)
         
